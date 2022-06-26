8ed7815a0638366c69c72aaa2072e9c2c65081ab
monicaliz <monicaeliz3@gmail.com>
2022-06-26 17:23:10 -0400

    commit tree 1df10e736236a2348ac4b58ac9d2a1bf986596f9

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
+// LAB INSTRUCTIONS
+// 1. Add event listeners to capture form data and override a form's default behavior
+// 2. Fetch data based on what the user types into that form
+// 3. Display that data on the page
+
+
 const init = () => {
-  
+  const inputForm = document.querySelector('form')
+  // #1 -- add event listener -- params: type of event + cb fn
+    inputForm.addEventListener('submit', (e) => {
+        //override submit
+        e.preventDefault()
+        // access input value
+        const input = document.querySelector('input#searchByID')
+// #2 -- basic fetch
+        // use back quotes for path
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(response => response.json() )
+            .then(data => {
+                const title = document.querySelector('section#movieDetails h4');
+                const summary = document.querySelector('section#movieDetails p')
+// #3 -- display value
+                title.innerText = data.title;
+                summary.innerText = data.summary;
+            })
+        })
 }
 
 document.addEventListener('DOMContentLoaded', init);
